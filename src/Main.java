ea16884218bff0cb965bc9d1639df8c7923dea56
niyamanka <mankaniya3@gmail.com>
2026-03-07 02:19:12 +0530

    UC13 - Performance Comparison of Palindrome Algorithms

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,71 @@
+import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Main {
+
+    // Reverse String Method
+    public static boolean reverseMethod(String word) {
+        String reversed = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.charAt(i);
+        }
+        return word.equals(reversed);
+    }
+
+    // Stack Method
+    public static boolean stackMethod(String word) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : word.toCharArray()) {
+            stack.push(c);
+        }
+
+        String reversed = "";
+        while (!stack.isEmpty()) {
+            reversed += stack.pop();
+        }
+
+        return word.equals(reversed);
+    }
+
+    // Deque Method
+    public static boolean dequeMethod(String word) {
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char c : word.toCharArray()) {
+            deque.add(c);
+        }
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Palindrome Checker");
+        String word = "AmanaplanacanalPanama";
 
-        String word = "madam";
+        // Reverse method timing
+        long start = System.nanoTime();
+        reverseMethod(word);
+        long end = System.nanoTime();
+        System.out.println("Reverse Method Time: " + (end - start) + " ns");
 
-        // Choose algorithm dynamically
-        PalindromeStrategy strategy = new StackStrategy();
-        // PalindromeStrategy strategy = new DequeStrategy();
+        // Stack method timing
+        start = System.nanoTime();
+        stackMethod(word);
+        end = System.nanoTime();
+        System.out.println("Stack Method Time: " + (end - start) + " ns");
 
-        PalindromeService service = new PalindromeService(strategy);
-
-        boolean result = service.check(word);
-
-        if (result) {
-            System.out.println(word + " is a Palindrome");
-        } else {
-            System.out.println(word + " is not a Palindrome");
-        }
+        // Deque method timing
+        start = System.nanoTime();
+        dequeMethod(word);
+        end = System.nanoTime();
+        System.out.println("Deque Method Time: " + (end - start) + " ns");
     }
 }
