b6e3fedd1870035f7fe518490de63fd561b25852
niyamanka <mankaniya3@gmail.com>
2026-03-07 02:15:07 +0530

    UC11 - Object Oriented Palindrome Service

--- src/Main.java
+++ src/Main.java
@@ -5,26 +5,11 @@ public class Main {
 
         String word = "A man a plan a canal Panama";
 
-        // String preprocessing
-        String cleaned = word.replaceAll("\\s+", "").toLowerCase();
+        PalindromeChecker checker = new PalindromeChecker();
 
-        boolean isPalindrome = true;
+        boolean result = checker.checkPalindrome(word);
 
-        int start = 0;
-        int end = cleaned.length() - 1;
-
-        // Compare characters
-        while (start < end) {
-            if (cleaned.charAt(start) != cleaned.charAt(end)) {
-                isPalindrome = false;
-                break;
-            }
-            start++;
-            end--;
-        }
-
-        // Display result
-        if (isPalindrome) {
+        if (result) {
             System.out.println(word + " is a Palindrome");
         } else {
             System.out.println(word + " is not a Palindrome");
